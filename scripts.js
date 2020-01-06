55711864f430e6d845715188a9819110b69edf20
joeymariano <joey.michalina.mariano@gmail.com>
2020-01-06 04:26:59 -0500

    added style

--- scripts.js
+++ scripts.js
@@ -1,21 +1,35 @@
+// create Show class
+// contains title imgUrl artist and rating
+// API call to data.json on window load
+// parse data and organize with Show class
+// populate template information with info from first show
+// iterate over shows
+// copy previous divs and repopulate
+
+class Show {
+  	constructor(title, imgUrl, artist, rating){
+  		this.title = title
+  		this.imgUrl = imgUrl
+  		this.artist = artist
+  		this.rating = rating
+  	}
+}
+
 window.onload = function(){
-	// API call to data.json
+	// api call to data.json
+	fetch('/data.json') 
+	  .then((response) => {
+	    return response.json()
+	  })
+	  .then((showJson) => {
+	    let sortedShows = showJson.Items.map(
+	    	obj => new Show(obj.Ttla.Line2, obj.ImageUrls[0].ImageUrl,obj.Ttla.Line1, obj.TvRating) 
+	    	)
 
-	// parse data 
-
-	// create Show class
-	// title imagUrl artist and rating
-
-	// populate template information with info from first show
-	// iterate over shows
-	// copy previous divs and repopulate
-
-	
-	// fetch('/data.json') 
-	//   .then((response) => {
-	//     return response.json();
-	//   })
-	//   .then((myJson) => {
-	//     console.log(myJson);
-	//   });
+	    // populate first template
+	    document.getElementsByTagName('img')[0].src = sortedShows[0].imgUrl
+	    document.getElementsByClassName('show-title')[0].textContent = sortedShows[0].title
+	    document.getElementsByClassName('show-artist')[0].textContent = sortedShows[0].artist
+	    document.getElementsByClassName('show-rating')[0].textContent = sortedShows[0].rating
+	  })
 }
