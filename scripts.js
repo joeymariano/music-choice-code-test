4837561058110159f74c696154b7f75ed59c10cf
joeymariano <joey.michalina.mariano@gmail.com>
2020-01-06 10:18:14 -0500

    iterates and clones cards

--- scripts.js
+++ scripts.js
@@ -25,10 +25,28 @@ window.onload = function(){
 	    let sortedShows = showJson.Items.map(
 	    	obj => new Show(obj.Ttla.Line2, obj.ImageUrls[0].ImageUrl,obj.Ttla.Line1, obj.TvRating) )
 
-	    // populate first template
-	    document.getElementsByTagName('img')[0].src = sortedShows[0].imgUrl
-	    document.getElementsByClassName('show-title')[0].textContent = `"` + sortedShows[0].title + `"`
-	    document.getElementsByClassName('show-artist')[0].textContent = sortedShows[0].artist
-	    document.getElementsByClassName('show-rating')[0].textContent = sortedShows[0].rating
+	    populateHtml(sortedShows)
 	  })
+}
+
+function populateHtml(sortedShows){
+	// populate first template
+
+
+	sortedShows.forEach(
+		(obj, index) => {
+			if (index === 0){
+				document.getElementsByTagName('img')[0].src = sortedShows[0].imgUrl
+		    document.getElementsByClassName('show-title')[0].textContent = `"` + sortedShows[0].title + `"`
+		    document.getElementsByClassName('show-artist')[0].textContent = sortedShows[0].artist
+		    document.getElementsByClassName('show-rating')[0].textContent = sortedShows[0].rating
+			} else {
+				let cardTemplate = document.getElementsByClassName('card')[0].cloneNode(true)
+				let cards = document.getElementsByClassName('cards')
+
+				cards[cards.length - 1].appendChild(cardTemplate)
+			}
+		}
+	)
+
 }
